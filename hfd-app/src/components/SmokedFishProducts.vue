5495357dfbc5904a50d32c028ea11f1cdd2d03e6
Dom Sinclair <dom@viewtolearn.co.uk>
2023-11-16 23:37:30 +0000

    added smoked fish and new json data

--- hfd-app/src/components/SmokedFishProducts.vue
+++ hfd-app/src/components/SmokedFishProducts.vue
@@ -14,31 +14,66 @@
 // const addToCart = (item, quantity /*, event*/) => {
 // 	emit('addToCart', { item, quantity })
 // }
-const products = [
+const SmokedFish = [
   {
-    id: 1,
-    name: "Basic Tee 8-Pack",
-    href: "#",
-    price: "$256",
-    description:
-      "Get the full lineup of our Basic Tees. Have a fresh shirt all week, and an extra for laundry day.",
-    options: "8 colors",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
-    imageAlt:
-      "Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.",
+    Id: 18,
+    Category: "Smoked Fish",
+    Name: "Salmon Fillet",
+    Description: "A whole side of smoked Salmon",
+    Options: "Sold by weight",
+    Price: "38.00",
+    UnitOfSale: "Kg",
+    ImageName: "/smokedsalmonfillet.jpg",
+    Visible: "Yes",
+    Href: "#",
   },
   {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    price: "$32",
-    description:
-      "Look like a visionary CEO and wear the same black t-shirt every day.",
-    options: "Black",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
-    imageAlt: "Front of plain black t-shirt.",
+    Id: 19,
+    Category: "Smoked Fish",
+    Name: "Hot Smoked Salmon",
+    Description: "Portions of hot smoked Salmon",
+    Options: "Sold by weight",
+    Price: "35.00",
+    UnitOfSale: "kg",
+    ImageName: "/hotsmokedsalmon.jpg",
+    Visible: "Yes",
+    Href: "#",
+  },
+  {
+    Id: 20,
+    Category: "Smoked Fish",
+    Name: "Smoked Mackerel",
+    Description: "SmokedMackerel fillets",
+    Options: "Sold by weight",
+    Price: "21.00",
+    UnitOfSale: "kg",
+    ImageName: "/smokedmackerelfillet.jpg",
+    Visible: "Yes",
+    Href: "#",
+  },
+  {
+    Id: 21,
+    Category: "Smoked Fish",
+    Name: "Smoked Cod",
+    Description: "Smoked Cod loins",
+    Options: "Sold by weight",
+    Price: "22.50",
+    UnitOfSale: "kg",
+    ImageName: "/smokedcodloin.jpg",
+    Visible: "Yes",
+    Href: "#",
+  },
+  {
+    Id: 22,
+    Category: "Smoked Fish",
+    Name: "Smoked Haddock",
+    Description: "Undyed smoked Haddock fillets",
+    Options: "Sold by weight",
+    Price: "22.50",
+    UnitOfSale: "kg",
+    ImageName: "/undyedsmokedhaddockfillet.jpg",
+    Visible: "Yes",
+    Href: "#",
   },
   // More products...
 ];
@@ -63,41 +98,41 @@ const products = [
     <div
       class="max-w-2xl px-4 py-16 mx-auto sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
     >
-      <h2 class="sr-only">Products</h2>
+      <h2 class="sr-only">SmokedFish</h2>
 
       <div
         class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8"
       >
         <div
-          v-for="product in products"
-          :key="product.id"
+          v-for="fish in SmokedFish"
+          :key="fish.Id"
           class="relative flex flex-col overflow-hidden bg-white border border-gray-200 rounded-lg dark:bg-gray-600 group"
         >
           <div
             class="bg-gray-200 aspect-h-4 aspect-w-3 sm:aspect-none group-hover:opacity-75 sm:h-96"
           >
             <img
-              :src="product.imageSrc"
-              :alt="product.imageAlt"
+              :src="fish.ImageName"
+              :alt="fish.ImageName"
               class="object-cover object-center w-full h-full sm:h-full sm:w-full"
             />
           </div>
           <div class="flex flex-col flex-1 p-4 space-y-2">
             <h3 class="text-sm font-medium text-gray-900 dark:text-gray-200">
-              <a :href="product.href">
+              <a :href="fish.Href">
                 <span aria-hidden="true" class="absolute inset-0" />
-                {{ product.name }}
+                {{ fish.Name }}
               </a>
             </h3>
             <p class="text-sm text-gray-500 dark:text-gray-300">
-              {{ product.description }}
+              {{ fish.Description }}
             </p>
             <div class="flex flex-col justify-end flex-1">
               <p class="text-sm italic text-gray-500 dark:text-gray-300">
-                {{ product.options }}
+                {{ fish.Options }}
               </p>
               <p class="text-base font-medium text-gray-900 dark:text-gray-200">
-                {{ product.price }}
+                £{{ fish.Price }} {{ fish.UnitOfSale }}
               </p>
             </div>
           </div>
